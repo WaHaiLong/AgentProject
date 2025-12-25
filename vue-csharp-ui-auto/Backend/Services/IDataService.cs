5d9f01e065d328e2cb5b3e5ce5553c7191f3d276
Wang Chuan Long <1724349716@qq.com>
2025-12-25 11:21:39 +0800

    chore: 从父仓库移除vue-csharp-ui-auto追踪

--- vue-csharp-ui-auto/Backend/Services/IDataService.cs
+++
@@ -1,9 +0,0 @@
-using Backend.Models;
-
-namespace Backend.Services
-{
-    public interface IDataService
-    {
-        Task<SubmitDataResponse> SubmitDataAsync(SubmitDataRequest request);
-    }
-}
